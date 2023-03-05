05a13e3f3c9e2c54d7aa35c2930441484bc13bb9
Audioaxel <audioaxel@audiolarks.com>
2023-03-05 21:22:01 +0100

    added WebApi

---
+++ 01_WebApi/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/", () => "test");
+
+
+app.UseCors("AllowAll");
+
+app.Run();
